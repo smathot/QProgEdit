493aa20c2284820f7ff75459371e8813b8aec134
Sebastiaan Mathot <s.mathot@cogsci.nl>
2014-05-13 16:27:07 +0200

    Improved find/ replace behavior

--- QProgEdit/_qeditorfind.py
+++ QProgEdit/_qeditorfind.py
@@ -45,45 +45,98 @@ class QEditorFind(QtGui.QWidget):
 		self.ui.pushButtonReplace.clicked.connect(self.replace)
 		self.ui.pushButtonReplaceAll.clicked.connect(self.replaceAll)
 		self.bestHeight = self.height()
-
+		self.matchNr = None
+		
+	def caseSensitive(self):
+		
+		"""
+		Returns:
+		True or False, depending on whether we should search case sensitive.
+		"""
+		
+		return self.ui.checkBoxCaseSensitive.isChecked()
+		
+	def editor(self):
+		
+		"""
+		Returns:
+		The QsciScintilla object.
+		"""
+		
+		return self.qProgEdit.editor
+	
+	def findText(self):
+		
+		"""
+		Returns:
+		The find text.
+		"""
+		
+		return self.ui.lineEditFind.text()
+	
 	def find(self):
 
 		"""
-		Finds in the document
+		Finds the current text in the document.
 
 		Returns:
-		True if matching text has been found, False otherwise
+		True if matching text has been found, False otherwise.
 		"""
-
-		return self.qProgEdit.editor.findFirst(self.ui.lineEditFind.text(), \
-			False, self.ui.checkBoxCaseSensitive.isChecked(), \
-			self.ui.checkBoxMatchWhole.isChecked(), True)
+		
+		return self.editor().findFirst(self.findText(), False,
+			self.caseSensitive(), self.matchWhole(), True)
+	
+	def matchWhole(self):
+		
+		"""
+		Returns:
+		True or False, depending on whether we should match whole words only.
+		"""
+		
+		return self.ui.checkBoxMatchWhole.isChecked()	
 
 	def replace(self):
 
 		"""
-		Replaces the first occurence in the document
+		Replaces the first occurence in the document.
 
 		Returns:
-		True if text has been replaces, False otherwise
+		True if text has been replaced, False otherwise.
 		"""
 
-		if self.ui.lineEditFind.text().toLower() in \
-			self.ui.lineEditReplace.text().toLower():
+		selection = self.editor().selectedText()
+		findText = self.findText()
+		if not self.caseSensitive():
+			selection = selection.toLower()
+			findText = findText.toLower()
+		if selection != findText and not self.find():
 			return False
-
-		if not self.find():
-			return False
-		self.qProgEdit.editor.replace(self.ui.lineEditReplace.text())
+		self.editor().replace(self.replaceText())
 		return True
 
 	def replaceAll(self):
 
-		"""Replace all occurences in the document"""
-
-		i = 0
-		while self.replace():
-			i += 1
+		"""Replace all occurences in the document."""
+		
+		self.editor().beginUndoAction()
+		text = QtCore.QString(self.editor().text())
+		if self.caseSensitive():
+			cs = QtCore.Qt.CaseSensitive
+		else:
+			cs = QtCore.Qt.CaseInsensitive
+		n = text.count(self.findText(), cs)
+		for i in range(n):
+			self.replace()
+		self.editor().endUndoAction()
+		
+	def replaceText(self):
+		
+		"""
+		Returns:
+		The replace text.
+		"""
+		
+		return self.ui.lineEditReplace.text()
 
 	def unshow(self):
 
