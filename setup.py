485fbda396aa8f37efb7b99e30f89a3ee69ad9ed
Sebastiaan Mathot <s.mathot@cogsci.nl>
2016-12-23 13:32:31 +0100

    Update setup script

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@
 # -*- coding: utf-8 -*-
 
 """
-This file is part of zoteromarkdown.
+This file is part of QProgEdit.
 
-zoteromarkdown is free software: you can redistribute it and/or modify
+QProgEdit is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-zoteromarkdown is distributed in the hope that it will be useful,
+QProgEdit is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with zoteromarkdown.  If not, see <http://www.gnu.org/licenses/>.
+along with QProgEdit.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 from QProgEdit import __version__
